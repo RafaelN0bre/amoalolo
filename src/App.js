ef5a9a5327c6337b75c91412294661671979746d
Rafael Nobre <rafaelmedeirosnobre2001@gmail.com>
2024-06-19 22:07:12 -0300

    feat: love quotes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import React, { useState } from 'react';
+
 import Counter from './components/Counter';
 import Footer from './components/Footer';
 import Galery from './components/Galery';
 import Nav from './components/Nav';
+import LoveQuotes from './components/LoveQuotes/LoveQuotes';
 
 function App() {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleSequenceMatch = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
   const targetDate = '2023-03-22T20:00:00';
 
   return (
     <div className="App">
-      <Nav/>
+      <LoveQuotes open={isModalOpen} onClose={handleCloseModal}/>
+      <Nav onSequenceMatch={handleSequenceMatch}/>
       <Counter targetDate={targetDate}/>
       <Galery />
       <Footer />
--- src/components/Galery.jsx
+++ src/components/Galery.jsx
@@ -21,7 +21,7 @@ const maps = [
   //   googlemapslink:
   //     "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d239.9099769262395!2d-47.97623933093806!3d-15.827157535010034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTXCsDQ5JzM3LjciUyA0N8KwNTgnMzQuNSJX!5e0!3m2!1spt-BR!2sbr!4v1718127177843!5m2!1spt-BR!2sbr",
   //   title: "🤓",
-  //   date: "22/03/2023",
+  //   date: "25/03/2023",
   // },
   {
     googlemapslink:
